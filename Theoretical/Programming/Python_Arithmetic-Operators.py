f767f9525a45c7524f6931320351c543c3b3b189
Dex <59426458+Dixboi@users.noreply.github.com>
2025-05-13 15:48:55 +0800

    Update Python_Arithmetic-Operators.py

--- Theoretical/Programming/Python_Arithmetic-Operators.py
+++ Theoretical/Programming/Python_Arithmetic-Operators.py
@@ -1,13 +1,15 @@
 '''
-Task
+Source:
+https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+Task:
 The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
 
 - The first line contains the sum of the two numbers.
 - The second line contains the difference of the two numbers (first - second).
 - The third line contains the product of the two numbers.
 
-Input Format
-
+Input Format:
 The first line contains the first integer, a.
 The second line contains the second integer, b.
 '''
